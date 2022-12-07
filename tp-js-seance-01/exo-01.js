e2bf5a9d2157c463d6a6eefb67ecbf0a2fa0a99c
Dylan-Kirsch <kirsch-dylan@bbox.fr>
2022-12-07 14:31:19 +0100

    seance 1 exo 1-5

---
+++ tp-js-seance-01/exo-01.js
@@ -0,0 +1,13 @@
+let prenom = prompt("Quel est votre prénom ? ");
+let age = prompt(" Quel est votre age ?");
+
+
+console.log("je m'appelle " + prenom );
+console.log("et j’ai " +age + " ans l’année prochaine");
+
+
+
+
+
+
+
--- tp-js/js/second.js
+++ tp-js/js/second.js
@@ -2,5 +2,6 @@ let prenom = prompt('Quel est votre prénom ?');
 let nom = prompt('Quel est votre nom ?');
 let age = prompt('Quel est votre age');
 let age_futur = parseInt(age) + 1 ;
+
 console.log('Bonjour ' + prenom +' '+ nom);
 console.log('Dans un an vous aurez ' + age_futur + ' ans');
