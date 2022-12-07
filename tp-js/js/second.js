c1744d59fa35b25790e4cc52050a1224b8fb6a6e
Dylan-Kirsch <kirsch-dylan@bbox.fr>
2022-12-07 11:26:00 +0100

    tp js

--- tp-js/js/second.js
+++ tp-js/js/second.js
@@ -1,6 +1,6 @@
-let prenom = 'Quel est votre prénom ?';
-let nom = 'Quel est votre nom ?';
-let age = 'Quel est votre age';
+let prenom = prompt('Quel est votre prénom ?');
+let nom = prompt('Quel est votre nom ?');
+let age = prompt('Quel est votre age');
 let age_futur = parseInt(age) + 1 ;
 console.log('Bonjour ' + prenom +' '+ nom);
 console.log('Dans un an vous aurez ' + age_futur + ' ans');
