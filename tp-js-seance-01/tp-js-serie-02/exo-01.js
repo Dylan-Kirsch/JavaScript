00de0527b2b030aeed973d268f6493f7ab5f9627
Dylan-Kirsch <kirsch-dylan@bbox.fr>
2022-12-08 09:39:11 +0100

    seance 2 js

---
+++ tp-js-seance-01/tp-js-serie-02/exo-01.js
@@ -0,0 +1,3 @@
+let bienvenue = prompt("Bienvenue, quel est ton prenom ?");
+
+console.log(bienvenue + " ");
--- tp-js-seance-02/exo-01.js
+++
@@ -1,3 +0,0 @@
-let bienvenue = prompt("Bienvenue, quel est ton prenom ?");
-
-console.log(bienvenue + " ");
