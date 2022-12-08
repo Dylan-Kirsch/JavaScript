2f5d1deab38a2e3f5f069875aeb5509290ef658f
Dylan-Kirsch <kirsch-dylan@bbox.fr>
2022-12-08 16:22:23 +0100

    exercice séance 2 série 2

---
+++ js-seance-02/js-s2-serie-02/exo-01.js
@@ -0,0 +1,31 @@
+// const pair = 'le nombre est paire';
+// const impair = 'le nombre est impair';
+
+// for(let i=1; i<11;i++) {
+
+//     if (i%2 == 0) {
+
+//         console.log(pair + ' ' + i);
+//     }
+//     else {
+
+//         console.log(impair + ' ' + i);
+
+//     }
+// }
+
+
+for(let i=1; i<11;i++) {
+
+    if (i%2 == 0) {
+
+        console.log('le nombre ' + i + ' est pair');
+
+    }
+    else {
+
+        console.log('le nombre ' + i + ' est impair');
+
+    }
+}
+
---
+++ js-seance-02/js-s2-serie-02/exo-02.js
@@ -0,0 +1,8 @@
+let x = parseInt(prompt('x est égal combien ? '));
+let n = parseInt(prompt('n est égal combien ? '));
+
+for (let i = 1 ; i < n ; i++ ) {
+
+    
+
+}
