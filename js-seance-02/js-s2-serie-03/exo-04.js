25b7a3cdf49783233ea9841c8e191bfe6697fb54
Dylan-Kirsch <kirsch-dylan@bbox.fr>
2022-12-14 11:23:19 +0100

    correction

--- js-seance-02/js-s2-serie-03/exo-04.js
+++ js-seance-02/js-s2-serie-03/exo-04.js
@@ -1,8 +1,46 @@
-let min=1; 
-let max=10;  
-let random = Math.floor(Math.random() * (max - min)); 
+//tirage aléatoire
+let nombreCherche = Math.round(Math.random() * (5-1) + 1);
+console.log(nombreCherche);
+let nombre = 0;
 
-let choisirNombre = parseInt(prompt('Choisir un nombre entre 1 et 10'));
+do {
+    // saisie du nombre
+    nombre = parseInt(prompt('proposez un nombre entre 1 et 5'));
+    // donner les indices <> si pas égal
+    if (nombre != nombreCherche) {
+        if (nombre > nombreCherche)
+            alert('trop grand');
+        else
+            alert('trop petit');
+    }
+}
+while ((nombre != nombreCherche))
+alert('BRAVO tu as trouvé!');
+// recommancer si pas égal 5
 
 
+/***** tirage 3 essais *****************************/ 
 
+//tirage aléatoire
+// let nombreCherche = Math.round(Math.random() * (5-1) + 1);
+// console.log(nombreCherche);
+// let nombre = 0;
+// let nbEssais = 0;
+// do {
+//     // saisie du nombre
+//     nombre = parseInt(prompt('proposez un nombre entre 1 et 5'));
+//     nbEssais+=1;
+//     // donner les indices <> si pas égal
+//     if (nombre != nombreCherche) {
+//         if (nombre > nombreCherche)
+//             alert('trop grand');
+//         else
+//             alert('trop petit');
+//     }
+// }
+// while ((nombre != nombreCherche)&&(nbEssais<3))
+// if (nombre == nombreCherche)
+//     alert('BRAVO tu as trouvé! en '+nbEssais+' tentatives');
+// else
+//     alert('BOOOUHHH TU ES NUL');
+// // recommancer si pas égal 5
